fa2c40700803bf10d5fa97e4c823a9eb962ec8ba
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-25 17:37:15 +0100

    Modified : comment file to post issues

--- src/comment.rs
+++ src/comment.rs
@@ -1,22 +1,22 @@
-// use reqwest::{Client, Error};
-// use serde::Serialize;
+use reqwest::{Client, Error};
+use serde::Serialize;
 
-// #[derive(Serialize)]
-// struct Comment {
-//     body: String,
-// }
+#[derive(Serialize)]
+struct Comment {
+    body: String,
+}
 
-// async fn post_comment(pr_number: u32, message: String) -> Result<(), Error> {
-//     let client = Client::new();
-//     let comment = Comment { body: message };
-//     client
-//         .post(format!(
-//             "https://api.github.com/repos/{owner}/{repo}/issues/{}/comments",
-//             pr_number
-//         ))
-//         .bearer_auth("GITHUB_TOKEN")
-//         .json(&comment)
-//         .send()
-//         .await?;
-//     Ok(())
-// }
+async fn post_comment(pr_number: u32, message: String) -> Result<(), Error> {
+    let client = Client::new();
+    let comment = Comment { body: message };
+    client
+        .post(format!(
+            "https://api.github.com/repos/{owner}/{repo}/issues/{}/comments",
+            pr_number
+        ))
+        .bearer_auth("GITHUB_TOKEN")
+        .json(&comment)
+        .send()
+        .await?;
+    Ok(())
+}
