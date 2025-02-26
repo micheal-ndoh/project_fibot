c144dc1e0f8f9e875ef95a72460e39d40cfe38f3
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-26 15:59:54 +0100

    Make post_comment function public and update parameters for flexibility

--- src/comment.rs
+++ src/comment.rs
@@ -7,9 +7,7 @@ struct Comment {
     body: String,
 }
 
-async fn post_comment(pr_number: u32, message: String) -> Result<(), Error> {
-    let owner = "micheal-ndoh";
-    let repo = "project_fibot";
+pub async fn post_comment(owner: &str, repo: &str, pr_number: u32, message: String) -> Result<(), Error> {
     let token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
     let client = Client::new();
     let comment = Comment { body: message };
