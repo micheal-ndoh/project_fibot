e13774f03d35954d688fcb96a536d09473aeadf4
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-26 16:01:38 +0100

    Fix: Update variable naming in fibonacci function

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,6 +1,6 @@
 pub fn fibonacci(n: u32, max_value: u32) -> Vec<u32> {
     let mut fib_sequence = vec![0, 1];
-    while let Some(&last) = fib_sequence.last() {
+    while let Some(&_last) = fib_sequence.last() {
         let next = fib_sequence[fib_sequence.len() - 1] + fib_sequence[fib_sequence.len() - 2];
         if next > max_value {
             println!("Reached maximum threshold value: {}", max_value);
