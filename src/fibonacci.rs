e262ce58e944c9419ce1eda8de3395e16aeb7e59
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-27 15:15:44 +0100

    updated fibonacci

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -10,24 +10,3 @@ pub fn fibonacci(n: u32, max_value: u32) -> Vec<u32> {
     }
     fib_sequence.into_iter().take(n as usize).collect()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_fibonacci_with_max_value() {
-        let max_value = 100;
-        let result = fibonacci(10, max_value);
-        let expected = vec![0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn test_fibonacci_exceeds_max_value() {
-        let max_value = 20;
-        let result = fibonacci(10, max_value);
-        let expected = vec![0, 1, 1, 2, 3, 5, 8, 13];
-        assert_eq!(result, expected);
-    }
-}
