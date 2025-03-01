56abbc00cfb3f149b6553fdb4e789153bfb97fa3
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-01 14:48:03 +0100

    update max threshold to be the nth value

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -10,7 +10,8 @@ pub fn fibonacci(n: u32, max_value: u32) -> u32 {
     let mut fib_sequence = vec![0, 1];
     while let Some(&_last) = fib_sequence.last() {
         let next = fib_sequence[fib_sequence.len() - 1] + fib_sequence[fib_sequence.len() - 2];
-        if next > max_value {
+        if n > max_value {
+            
             break;
         }
         fib_sequence.push(next);
