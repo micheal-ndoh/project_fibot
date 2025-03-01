5e9194698880b984fc8eb1e472224955b5843996
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-01 15:26:58 +0100

    update fibonacci

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,4 +1,4 @@
-pub fn fibonacci(n: u32, max_value: u32) -> u32 {
+pub fn fibonacci(n: u32) -> u32 {
     if n == 0 {
         return 0;
     }
@@ -10,7 +10,7 @@ pub fn fibonacci(n: u32, max_value: u32) -> u32 {
     let mut fib_sequence = vec![0, 1];
     while let Some(&_last) = fib_sequence.last() {
         let next = fib_sequence[fib_sequence.len() - 1] + fib_sequence[fib_sequence.len() - 2];
-        if n > max_value {
+        if next > n {
             
             continue;
         }
